d58b16257abab07e60390659fccdba143791a560
WillAndGo <wilmer.gonzalez@uninorte.edu.co>
2022-12-01 23:11:36 -0300

    instaYa

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,54 +1,87 @@
-export default function Login(){
+export default function Login1(){
 
     return (
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-xl-20 col-lg-12 col-md-9">
-                    <div class="card o-hidden border-5 shadow-lg my-5">
-                        <div class="card-body p-4">
-                            {/* <!-- Nested Row within Card Body --> */}
-                            <div class="row">
-                                <div class="col-lg-6 d-none d-lg-block ">
-                                    <img src="https://www.imagar.com/wp-content/uploads/2020/07/Desarrollo-web-ecommerce-1.jpg" class="img-fluid" alt="Responsive image" /></div>
-                                <div class="col-lg-6">
-                                    <div class="p-0">
-                                        <div class="text-center">
-                                        <h1 class="h1 text-black-900 mb-2">Bienvenido</h1>
-                                        </div>
-                                        {/* <!--Formulario de Login--> */}
-                                        <from>
-                                            <div class="mb-1">
-                                                <label for="exampleInputEmail1" class="form-label">Ingrese su Correo</label>
-                                                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                                                <div id="emailHelp" class="form-text">Nunca compartiremos su correo electrónico con nadie más.</div>
-                                            </div>
-                                            <div class="mb-2">
-                                                <label for="exampleInputPassword1" class="form-label">Ingrese su Contraseña</label>
-                                                <input type="password" class="form-control" id="exampleInputPassword1" />
-                                            </div>
-                                            
-                                            <div class="mb-1 col-lg-3 d-grid">
-                                                <button type="submit" class="btn btn-primary">Iniciar Sesión</button>
-                                            </div>
-                                            <div class="mb-1 col-md-4 from-check">
-                                                <input type="checkbox" class="form-check-input" id="exampleCheck1"></input>
-                                                <label class="form-check-label" for="exampleCheck1">Recordar Contraseña</label>
-                                            </div>
-                                            <div class="my-1 col-md-6">
-                                                <span>¿No tienes una cuenta?<a href="#"> Registrate Aquí</a></span><br/>
-                                                <span><a href="#">Recuperar Contraseña</a></span>
-                                                </div>
-                                        </from>
-                                        </div>
+        <div class="row justify-content-center">
+            <div class="col-xl-10 col-lg-12 col-md-9">
+                <div class="card o-hidden border-0 shadow-lg my-5">
+                    <div class="card-body p-0">
+                            
+                        <div class="row">
+                            <div class="col-lg-6 d-none d-lg-block ">
+                                <img src="https://www.imagar.com/wp-content/uploads/2020/07/Desarrollo-web-ecommerce-1.jpg" class="img-fluid" alt="Responsive image" /></div>
+                            <div class="col-lg-6">
+                                <div class="p-5">
+                                    <div class="text-center">
+                                        <h1 class="h4 text-gray-900 mb-4">Recogida de Paquetes</h1>
                                     </div>
-                                    {/* <div class="col"></div> */}
+
+                        
+                                    {/* <div class="alert alert-dismissible alert-info">
+                                        <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+                                        <h4 class="alert-heading">Ingreso al Sistema</h4>
+                                            <p class="mb-0">                                      
+                                            </p>
+                                    </div> */}
+                        
+                               
+
+                                        {/* <!--Formulario de Login--> */}
+                                    <form class="user" action="/validarlogin" method="POST">
+                                        <div class="form-group mb-3">
+                                            <input type="email" class="form-control form-control-user" name="usuario" aria-describedby="emailHelp" placeholder="Ingrese el Usuario"/>
+                                        </div>
+                                        <div class="form-group mb-3">
+                                            <input type="password" class="form-control form-control-user" id="exampleInputPassword" name="passw" placeholder="Contraseña"/>
+                                        </div>                                        
+                                        <div class="form-group mb-3">
+                                        <div class="custom-control custom-checkbox small">
+                                            <input type="checkbox" class="custom-control-input" id="customCheck"/>
+                                            <label class="custom-control-label" for="customCheck">Recordar</label>
+                                        </div>
+                                        </div>
+                                        <button class="btn btn-outline-primary btn-block" id="alertas" type="submit">Enviar</button>
+                        
+                        
+                                               
+                                    </form>
+                                            
+                                            
+                                    <div class="text-center">
+                                    <a class="small" href="#">Olvido su Contraseña?</a>
+                                    </div>
+                                            
+                                    <hr/>
+                                        <div class="text-center">
+                                        <a class="btn btn-outline-primary" href="{{url_for('registro')}}">Registrar Usuario</a>
+                                        </div>
+                    
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
+    
             </div>
-        /* </div> */
+            <div id="layoutAuthentication_footer">
+                <footer class="py-4 bg-light mt-auto">
+                    <div class="container-fluid px-4">
+                        <div class="d-flex align-items-center justify-content-between small">
+                            <div class="text-muted">Derechos Reservados &copy; Grupo 10 UniNorte</div>
+                            <div>
+                                <a href="#">Política de Privacidad</a>
+                                &middot;
+                                <a href="#">Términos & Condiciones</a>
+                            </div>
+                        </div>
+                    </div>
+                </footer>
+            </div>
+    
+        </div>
+    
+    /* </div> */
+  
+
 
     )
 }
